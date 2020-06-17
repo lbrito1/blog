f8f8289e669426f8572de806d00c4c8dea85e924
Leonardo Brito <lbrito@gmail.com>
2020-06-17 19:50:25 -0300

    Builds blog. (automated commit)

--- blog/assets/sane-tracker.js
+++ blog/assets/sane-tracker.js
@@ -1,9 +1,9 @@
 console.log("Hello! Not to worry, I'm just getting the userAgent, location and IP for this request. You can check the code yourself at https://github.com/lbrito1/sane-tracker-js. Happy coding!")
-const HOSTNAME = "https://android-analytics.duckdns.org/"
+const HOSTNAME = "https://android-analytics.duckdns.net/"
 
 function sane_track() {
   fetch(HOSTNAME, {
-    body: "userAgent=" + navigator.userAgent + "&url=" + window.location,
+    body: "token=M1mI0c53WcDu2FD0EIvQOdNlS9gWQtyh&url=" + window.location,
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     method: "POST"
   })
